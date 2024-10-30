30bdd40880a5476c9a55255de32c56d019c8f613
gabrielleadvyz <gabrielle@advyz.io>
2024-10-30 08:30:35 -0400

    Update and rename vite.config.js to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
-Copyimport { defineConfig } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000
-  },
   build: {
     outDir: 'dist'
   }
